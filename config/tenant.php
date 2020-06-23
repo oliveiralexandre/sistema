b493b932070f1859553eb543cf1b1f45aa54f3f1
oliveiralexnadre <oliveiralexandre0@gmail.com>
2020-06-22 22:59:00 -0300

    FIles

---
+++ config/tenant.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /**
+     * Admins e-mails
+     */
+    'admins' => [
+        'oliveiralexandre0@gmail.com',
+        'carlos@especializati.com.br',
+        'admin@especializati.com.br',
+    ],
+
+];
